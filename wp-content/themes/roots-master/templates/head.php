1cfd5b1d4340707339e1567d0fe4c46230d045d4
trungjc <trungnguyenthanh70@gmail.com>
2014-04-18 16:20:47 +0700

    update slideshow

--- wp-content/themes/roots-master/templates/head.php
+++ wp-content/themes/roots-master/templates/head.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0">
 
   <?php wp_head(); ?>
 <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7329292/732424/css/fonts.css" />
