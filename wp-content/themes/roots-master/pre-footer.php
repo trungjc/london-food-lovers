9bf707f2e966f017964024e4ca1e52f7137746c7
trungjc <trungnguyenthanh70@gmail.com>
2014-04-25 11:12:06 +0700

    test continue

--- wp-content/themes/roots-master/pre-footer.php
+++ wp-content/themes/roots-master/pre-footer.php
@@ -1,5 +1,5 @@
 <div class="pre-footer">  
-    <div class="container pre-footer-co">
+    <div class="container pre-footer-conatin">
         <?php dynamic_sidebar('bottom-widget'); ?>        
         <!--   <div class="row">
          <div class="col-md-6 col-xs-12">
