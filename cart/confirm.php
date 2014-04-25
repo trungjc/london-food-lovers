09407d5db79a02f777097ba2d6eb6543a900fb3c
Vipin Garg <vipin.garg@infoshore.biz>
2014-04-25 15:47:47 +0530

    redirect fix

--- cart/confirm.php
+++ cart/confirm.php
@@ -2,10 +2,7 @@
 
 include_once '../wp-load.php';
 
-include_once 'func.php';
-include 'lib/foodloverstours.php';
-
-$_SESSION['data'] = array_merge($_SESSION['data'],$_POST);
+$_SESSION['data'] = $_POST;
 
 $url = home_url()."/checkout-confirm/";
 header("Location:$url");
