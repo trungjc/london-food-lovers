4173c897272f326fe0c148cfcdfd770c108d8967
trungjc <trungnguyenthanh70@gmail.com>
2014-06-11 13:45:57 +0700

    update social icons in footer

--- wp-content/themes/roots-master/templates/footer.php
+++ wp-content/themes/roots-master/templates/footer.php
@@ -11,7 +11,14 @@
 			<br>
 			<div class="row bg-line">
 				<div class="col-md-12 center">
-					<h3>  London Food Tours</h3>
+				<h3>  London Food Tours</h3>
+					<div class="social-container" style="margin: 0 0 10px">
+						<a style="margin:5px"  href="https://www.facebook.com/pages/London-Food-Lovers/864626370344634"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/fb.png" /></a>
+						<a style="margin:5px" href="https://twitter.com/FoodLoversTours"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitt.png" /></a>
+						<a style="margin:5px" href="https://plus.google.com/112092265692699836505/posts"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/google.png" /></a>
+						<a style="margin:5px" href="mailto:info@londonfoodlovers.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/email.png" /></a>
+					</div>
+					
 					<p>
 				London Food Lovers Food Tours are rated among the top London Attractions, London Tours, London Private Tours, London Group Tours and London Food Tours. 
 					</p>
